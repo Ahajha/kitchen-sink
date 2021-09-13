129a67c5cd919ecd6826d4556f3d1382833c26ea
Alex Trotta <ahajha@gmail.com>
2021-09-13 05:14:52 -0400

    fibb() should take an unsigned

--- include/world.hpp
+++ include/world.hpp
@@ -4,4 +4,4 @@ void say_hello();
 
 // Returns the nth fibbonacci number. Here, the sequence is
 // defined for the 0th and 1st elements are 0 and 1, respectively.
-unsigned long long fibb(int n);
+unsigned long long fibb(unsigned n);
